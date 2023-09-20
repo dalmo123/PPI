456d80f66da6e5bc993a05350133fcdf7f5056f8
dalmo123 <66819868+dalmo123@users.noreply.github.com>
2023-09-20 15:30:08 -0300

    Add files via upload

---
+++ ExerciciosBootstrap/ex01/js/index.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    const form = document.getElementById("login-form");
+
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
+
+        const emailError = document.getElementById("email-error");
+        const senhaError = document.getElementById("senha-error");
+
+        emailError.textContent = "";
+        senhaError.textContent = "";
+
+        document.getElementById("email").classList.remove("error-border");
+        document.getElementById("senha").classList.remove("error-border");
+
+        if(email === ""){
+            emailError.textContent = "O email deve ser preenchido.";
+            document.getElementById("email").classList.add("error-border");
+            emailError.classList.remove("d-none");
+        }else{
+            emailError.classList.add("d-none");
+        }
+
+        if(senha === ""){
+            senhaError.textContent = "A senha deve ser preenchida.";
+            document.getElementById("senha").classList.add("error-border");
+            senhaError.classList.remove("d-none");
+        }else{
+            senhaError.classList.add("d-none");
+        }
+    });
+
+});
